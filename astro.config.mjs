8114f7ccebb9572abbe6dcaea0f201e3efcceb39
chris <ngmi@posteo.es>
2024-01-18 15:42:41 +0100

    alice blast demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
-import compress from 'astro-compress';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/config.mjs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -40,16 +39,6 @@ export default defineConfig({
         },
       })
     ),
-    compress({
-      css: true,
-      html: {
-        removeAttributeQuotes: false,
-      },
-      img: false,
-      js: true,
-      svg: false,
-      logger: 1,
-    }),
   ],
   vite: {
     resolve: {
