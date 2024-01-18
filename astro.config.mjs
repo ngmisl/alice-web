8b961a4bc448d04a87402be37b4517e9287d16f6
chris <ngmi@posteo.es>
2024-01-18 15:23:44 +0100

    alice blast demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/config.mjs';
-import rome from 'astro-rome';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const whenExternalScripts = (items = []) =>
   SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
@@ -51,7 +50,6 @@ export default defineConfig({
       svg: false,
       logger: 1,
     }),
-    rome(),
   ],
   vite: {
     resolve: {
